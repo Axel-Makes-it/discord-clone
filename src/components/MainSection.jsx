638468eba4576d006f9b275ddf3efc1c01c2d1c0
Axel Ivan Santiago <axelivanlux@gmail.com>
2023-10-03 17:01:52 -0400

    update

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import "../styles/MainSection.css";
 import Nav from "../components/Nav";
 import Hero from "../components/Hero";
+import leftIMG from "../images/leftIMG.svg";
+import rightIMG from "../images/rightIMG.svg";
 
 function MainSection() {
   return (
     <section className="mainSection__section">
+      <img className="leftIMG" src={leftIMG} alt="ICON" />
       <Nav />
       <Hero />
+      <img className="rightIMG" src={rightIMG} alt="ICON" />
     </section>
   );
 }
