38822c0c55c9af9503c549449fdfa310078c01d3
Axel Ivan Santiago <axelivanlux@gmail.com>
2023-10-03 22:01:53 -0400

    update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 import "../styles/Nav.css";
 import LOGO from "../images/Logo.svg";
+import MENU from "../images/menu.svg";
 
 function Nav() {
   return (
     <section className="nav__content">
       <nav>
         <img src={LOGO} alt="logo" />
-
         <ul>
           <li>Download</li>
           <li>Nitro</li>
@@ -17,8 +17,10 @@ function Nav() {
           <li>Blog</li>
           <li>Careers</li>
         </ul>
-
-        <div className="nav__login-btn">Open Discord</div>
+        <div className="nav__action">
+          <div className="nav__login-btn">Open Discord</div>
+          <img className="nav__menu-img" src={MENU} alt="Menu" />
+        </div>
       </nav>
     </section>
   );
