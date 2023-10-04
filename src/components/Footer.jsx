8dddf3aa668213328fce0fd8c3a32ad1a5a4778c
Axel Ivan Santiago <axelivanlux@gmail.com>
2023-10-03 21:23:12 -0400

    update

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -31,42 +31,6 @@ function Footer() {
               </li>
             </ul>
           </div>
-          <div className="footer__cat">
-            <h4>Product</h4>
-            <span>Download</span>
-            <span>Nitro</span>
-            <span>Status</span>
-            <span>App Directory</span>
-          </div>
-          <div className="footer__cat">
-            <h4>Company</h4>
-            <span>About</span>
-            <span>Jobs</span>
-            <span>Brand</span>
-            <span>Newsroom</span>
-          </div>
-          <div className="footer__cat">
-            <h4>Resources</h4>
-            <span>College</span>
-            <span>Support</span>
-            <span>Safety</span>
-            <span>Blog</span>
-            <span>Feedback</span>
-            <span>Developers</span>
-            <span>StreamKit</span>
-            <span>Creators</span>
-            <span>Community</span>
-          </div>
-          <div className="footer__cat">
-            <h4>Policies</h4>
-            <span>Terms</span>
-            <span>Privacy</span>
-            <span>Cookie Settings</span>
-            <span>Guelines</span>
-            <span>Acknowlegements</span>
-            <span>Licenses</span>
-            <span>Company Information</span>
-          </div>
         </div>
         <div className="footer__signup">
           <img src={LOGO} alt="" />
