b6cefac4fb981542fd0b5f1d3bfc0dbc2d922325
Axel Ivan Santiago <axelivanlux@gmail.com>
2023-10-05 00:24:07 -0400

    update

---
+++ src/components/DashChatLog.jsx
@@ -0,0 +1,67 @@
+import React from "react";
+import "../styles/DashChatLog.css";
+
+function DashChatLog() {
+  return (
+    <section className="dashChatLog__section">
+      <div className="dashChatLog__container">
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hey</div>
+        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">hi</div>
+        {/* ... Add more chat messages here */}
+      </div>
+    </section>
+  );
+}
+
+export default DashChatLog;
--- src/components/DashRightBar.jsx
+++ src/components/DashRightBar.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import "../styles/DashRightBar.css";
 import DashChatTop from "./DashChatTop";
+import DashChatLog from "./DashChatLog";
 
 function DashRightBar() {
   return (
     <section className="DashRightBar__section">
       <DashChatTop />
+      <DashChatLog />
     </section>
   );
 }
