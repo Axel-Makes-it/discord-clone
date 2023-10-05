ef3887bb242fd108bce21a019c7ad43166e0038c
Axel Ivan Santiago <axelivanlux@gmail.com>
2023-10-05 01:24:42 -0400

    update

--- src/components/DashChatLog.jsx
+++ src/components/DashChatLog.jsx
@@ -5,59 +5,45 @@ function DashChatLog() {
   return (
     <section className="dashChatLog__section">
       <div className="dashChatLog__container">
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hey</div>
-        <div className="dashChatLog__message">hi</div>
-        <div className="dashChatLog__message">hi</div>
+        <div className="dashChatLog__message">
+          <p>sup</p>
+        </div>
+        <div className="dashChatLog__message">
+          <p>sup</p>
+        </div>
+        <div className="dashChatLog__message">
+          <p>sup</p>
+        </div>
+        <div className="dashChatLog__message">
+          <p>sup</p>
+        </div>
+        <div className="dashChatLog__message">
+          <p>sup</p>
+        </div>
+        <div className="dashChatLog__message">
+          <p>sup</p>
+        </div>
+        <div className="dashChatLog__message">
+          <p>sup</p>
+        </div>
+        <div className="dashChatLog__message">
+          <p>sup</p>
+        </div>
+        <div className="dashChatLog__message">
+          <p>sup</p>
+        </div>
+        <div className="dashChatLog__message">
+          <p>sup</p>
+        </div>
+        <div className="dashChatLog__message">
+          <p>sup</p>
+        </div>
+        <div className="dashChatLog__message">
+          <p>sup</p>
+        </div>
+        <div className="dashChatLog__message">
+          <p>sup</p>
+        </div>
       </div>
     </section>
   );
