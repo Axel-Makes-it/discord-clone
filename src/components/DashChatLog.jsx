677dfdbed746a35b22b8fc69ac184eccd7299c32
Axel Ivan Santiago <axelivanlux@gmail.com>
2023-10-05 19:25:26 -0400

    update

--- src/components/DashChatLog.jsx
+++ src/components/DashChatLog.jsx
@@ -1,58 +1,43 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles/DashChatLog.css";
 
 function DashChatLog() {
+  const [value, setValue] = useState("");
+  const [messages, setMessages] = useState([]);
+
+  const handleInputChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (value.trim() !== "") {
+      setMessages([...messages, value]);
+      setValue("");
+    }
+  };
+
   return (
     <section className="dashChatLog__section">
       <div className="dashChatLog__container">
-        <div className="dashChatLog__message">
-          <p>sup</p>
-        </div>
-        <div className="dashChatLog__message">
-          <p>sup</p>
-        </div>
-        <div className="dashChatLog__message">
-          <p>sup</p>
-        </div>
-        <div className="dashChatLog__message">
-          <p>sup</p>
-        </div>
-        <div className="dashChatLog__message">
-          <p>sup</p>
-        </div>
-        <div className="dashChatLog__message">
-          <p>sup</p>
-        </div>
-        <div className="dashChatLog__message">
-          <p>sup</p>
-        </div>
-        <div className="dashChatLog__message">
-          <p>sup</p>
-        </div>
-        <div className="dashChatLog__message">
-          <p>sup</p>
-        </div>
-        <div className="dashChatLog__message">
-          <p>sup</p>
-        </div>
-        <div className="dashChatLog__message">
-          <p>sup</p>
-        </div>
-        <div className="dashChatLog__message">
-          <p>sup</p>
-        </div>
-        <div className="dashChatLog__message">
-          <p>sup</p>
-        </div>
-        <div className="dashChatLog__message">
-          <p>sup</p>
-        </div>
-        <div className="dashChatLog__message">
-          <p>sup</p>
-        </div>
-        <div className="dashChatLog__message">
-          <p>sup</p>
-        </div>
+        {messages.map((message, index) => (
+          <div className="dashChatLog__message" key={index}>
+            <p>{message}</p>
+          </div>
+        ))}
+      </div>
+      <div className="dashChatLogInput">
+        <form className="chatLogForm" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="Message"
+            value={value}
+            onChange={handleInputChange}
+          />
+          <button className="chatLog-btn" type="submit">
+            Send
+          </button>
+        </form>
       </div>
     </section>
   );
--- src/components/DashChatTop.jsx
+++ src/components/DashChatTop.jsx
@@ -9,7 +9,7 @@ function DashChatTop() {
       <div className="dashChatTop__info">
         <div className="dashChatTop__room">
           <img src={tag} alt="tag" />
-          <span>No-Chats-Rooms</span>
+          <span>Chat-Room-Name</span>
         </div>
         <div className="dashChatTop__trash" title="Delete Room">
           <img src={trash} alt="delete" />
